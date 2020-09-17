794ecbccaac26e616c2ce3a657a1d18369b93905
Bjarki Freyr <bjarkif19@ru.is>
2020-09-17 15:40:32 +0000

    def commit

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,3 +1,17 @@
+def moves(x_positon: int, y_positon: int, direction: str):
+    ''' Shows how you move in the game'''
+
+    if direction.lower() == 'n':
+        return x_positon, y_positon + 1
+    elif direction.lower() == 's':
+        return x_positon, y_positon - 1
+    elif direction.lower() == 'e':
+        return x_positon + 1, y_positon
+    elif direction.lower() == 'w':
+        return x_positon - 1, y_positon
+        # það þarf svo bara að setja þetta saman við while lykkjuna fyrir ofan
+
+
 location = "1,1"
 while location != "3,1":
     if location == "1,1":
@@ -20,3 +34,15 @@ while location != "3,1":
         else:
             print("Not a valid direction!")
 
+def moves(x_positon: int, y_positon: int, direction: str):
+    ''' Shows how you move in the game'''
+
+    if direction.lower() == 'n':
+        return x_positon, y_positon + 1
+    elif direction.lower() == 's':
+        return x_positon, y_positon - 1
+    elif direction.lower() == 'e':
+        return x_positon + 1, y_positon
+    elif direction.lower() == 'w':
+        return x_positon - 1, y_positon
+        # það þarf svo bara að setja þetta saman við while lykkjuna fyrir ofan
